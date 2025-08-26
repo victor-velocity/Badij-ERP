04f2b5cedacca7b5bf188f8eba21b0ddf5f7c47a
Asama Victor <asamavictor225@gmail.com>
2025-08-26 23:17:06 +0100

    corrected notification page

--- src/app/(dashboard)/humanResources/notifications/page.jsx
+++ src/app/(dashboard)/humanResources/notifications/page.jsx
@@ -1,6 +1,10 @@
-export const Page = () => {
-    return (
-        <>
-        </>
-    )
+"use client"
+
+export default function NotificationsPage() {
+  return (
+    <div>
+      <h1>Notifications</h1>
+      <p>Simple page without interactions</p>
+    </div>
+  );
 }
